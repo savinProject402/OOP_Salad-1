c4edd59be49129d21c30c04daf2e9ca783806bee
Олег Савін <oleh.savin@nure.ua>
2020-11-30 02:22:41 +0200

    Class Vegetables and Added for Stuffing

---
+++ Vegetables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Salad_1_
+{
+    public class Vegetables : Components_Abs
+    {
+            public Vegetables(double weight, string name, string type, int calories, double prise, string stuffing) 
+            : base (weight, name, type, calories, prise)
+        {
+            Stuffing = stuffing;
+        }
+        public string Stuffing { get; private set; }
+    }
+}
